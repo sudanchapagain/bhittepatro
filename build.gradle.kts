6ec326a50e0323d9420511a172b08e18353e8c70
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2026-08-22 22:53:21 +0545

    CODEBERG
    Signed-off-by: Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>

--- build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.androidMultiplatformLibrary) apply false
-    alias(libs.plugins.composeCompiler) apply false
-    alias(libs.plugins.composeMultiplatform) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply false
-    alias(libs.plugins.kotlinxSerialization) apply false
-    alias(libs.plugins.kmpNativeCoroutines) apply false
-}
--- iosApp/iosApp/ListView.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-struct ListView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
-}
